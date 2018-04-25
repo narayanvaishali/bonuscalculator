a8c389c93e2426022e037443df27bc202177e93f
narayanvaishali <narayanvaishali@gmail.com>
2018-04-25 14:38:00 +0100

    Add files via upload

--- src/components/branches.js
+++ src/components/branches.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
 import Message from './addbranch';
+import ModalBranch from './ModalBranch';
 import _ from 'lodash';
 
 class Branches extends Component {
 
   constructor(props){
     super(props);
+    this.saveModalDetails = this.saveModalDetails.bind(this);
+    this.replaceModalItem = this.replaceModalItem.bind(this);
     this.state = {
-      branches: []
-    };
+      requiredItem: 0,
+      branches: [],
+      showWarning: false
+    }
+  }
+  replaceModalItem(index) {
+    this.setState({
+      requiredItem: index,
+      showWarning : true
+    });
   }
   componentDidMount() {
     const itemsRef = this.props.db.database().ref('branches');
@@ -31,28 +42,93 @@ class Branches extends Component {
     const itemRef = this.props.db.database().ref(`/branches/${itemId}`);
     itemRef.remove();
   }
-
+  saveModalDetails(item, itemid) {
+    var updates = {};
+       const itemRef = this.props.db.database().ref(`/branches/${itemid}`);
+       itemRef.update(item);
+  }
 render(){
+
+  const branchlist = this.state.branches.map((branch, index) => {
     return (
-      <div>
-      <table border="1" className="table table-striped table-custom">
-        <tr>
-            <td ><b>Branch</b></td>
-            <td><b>Target</b></td>
-            <td>&nbsp;</td>
-        </tr>
-          {this.state.branches.map((branch) => {
-            return (
-              <tr>
-                  <td>{branch.branchname}</td>
-                  <td>{branch.target}</td>
-                  <td><button onClick={() => this.removeBranch(branch.id)}><span class="glyphicon glyphicon-trash"></span></button></td>
+
+              <tr key={index}>
+                      <td>{branch.branchname}</td>
+                      <td>{branch.target}</td>
+                      <td>
+                        <button className="btn btn-primary" data-toggle="modal" data-target="#exampleModal"
+                          onClick={() => this.replaceModalItem(index)}>edit</button> {" "}
+                      </td>
+                      <td>
+                        <td><button onClick={() => this.removeBranch(branch.id)}>
+                        <span class="glyphicon glyphicon-trash"></span></button></td>
+                      </td>
               </tr>
-            )
-          })}
+      )
+  });
+  const warn =this.state.showWarning;
+  let modalData;
+  if (warn)
+  {
+    const requiredItem = this.state.requiredItem;
+    modalData = this.state.branches[requiredItem];
+    //console.log(modalData);
+  }
+  return (
+
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <h1>Branch List</h1>
+      </div>
+      <table className="table table-striped">
+        <tbody>
+          <tr>
+              <td ><b>Branch</b></td>
+              <td><b>Target</b></td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+          </tr>
+          {branchlist}
+        </tbody>
       </table>
+
+      {warn ? (
+        <ModalBranch
+           branchname= {modalData.branchname}
+           target = {modalData.target}
+           id = {modalData.id}
+           saveModalDetails={this.saveModalDetails}
+        />
+            ) : (
+              console.log ('no data found')
+            )}
+    </div>
+  );
+
+  /*  return (
+      <div>
+          <table border="1" className="table table-striped table-custom">
+            <tr>
+                <td ><b>Branch</b></td>
+                <td><b>Target</b></td>
+                <td>&nbsp;</td>
+            </tr>
+              {this.state.branches.map((branch) => {
+                return (
+                  <tr>
+                      <td>{branch.branchname}</td>
+                      <td>{branch.target}</td>
+                      <td>
+                        <button className="btn btn-primary" data-toggle="modal" data-target="#exampleModal"
+                          onClick={() => this.replaceModalItem(index)}>edit</button> {" "}
+                      </td>
+                      <td><button onClick={() => this.removeBranch(branch.id)}><span class="glyphicon glyphicon-trash"></span></button></td>
+                  </tr>
+                )
+              })}
+          </table>
         </div>
-    )
+    )*/
   }
 }
 
--- src/components/stafflist.js
+++ src/components/stafflist.js
@@ -1,107 +1,132 @@
 import React, {Component} from 'react';
 import Message from './addbranch';
+import Modal from './Modal';
 import _ from 'lodash';
 
 class StaffList extends Component {
- //const  editmode : false;
-  constructor(props){
-
+  constructor(props) {
     super(props);
-    this.state = {
-      stafflist: []
-    };
-  }
-  componentDidMount() {
-    const itemsRef = this.props.db.database().ref('staff');
 
-    itemsRef.on('value', (snapshot) => {
-      let stafflist = snapshot.val();
-      //  console.log(stafflist);
-      let newState = [];
-      for (let staff in stafflist) {
-        newState.push({
-          id: staff,
-          initials: stafflist[staff].initials,
-          name : stafflist[staff].name,
-          designation : stafflist[staff].designation,
-          email : stafflist[staff].email,
-          phone : stafflist[staff].phone,
-          code : stafflist[staff].code
-        });
-      }
-      this.setState({
-        stafflist: newState
-      });
+    this.replaceModalItem = this.replaceModalItem.bind(this);
+    this.saveModalDetails = this.saveModalDetails.bind(this);
+    this.state = {
+      requiredItem: 0,
+      stafflist: [],
+      showWarning: false
+  }
+}
+
+  replaceModalItem(index) {
+    this.setState({
+      requiredItem: index,
+      showWarning : true
     });
   }
   removeBranch(itemId) {
-    const itemRef = this.props.db.database().ref(`/staff/${itemId}`);
-    itemRef.remove();
+     const itemRef = this.props.db.database().ref(`/staff/${itemId}`);
+     itemRef.remove();
+   }
+
+  saveModalDetails(item, itemid) {
+    var updates = {};
+       const itemRef = this.props.db.database().ref(`/staff/${itemid}`);
+       itemRef.update(item);
   }
 
-  editBranch(editstaff) {
-      console.log(editstaff);
-      //this.props.editStaff= staff;
-    //const itemRef = this.props.db.database().ref(`/staff/${staff.ID}`);
-    let editState = [];
-  //  for (let st in editstaff) {
-      editState.push({
-        id: editstaff.id,
-        initials: editstaff.initials,
-        name : editstaff.name,
-        designation : editstaff.designation,
-        email : editstaff.email,
-        phone : editstaff.phone,
-        code : editstaff.code
+  deleteItem(index) {
+    let tempBrochure = this.state.brochure;
+    tempBrochure.splice(index, 1);
+    this.setState({ brochure: tempBrochure });
+  }
+  componentDidMount() {
+      const itemsRef = this.props.db.database().ref('staff');
+
+      itemsRef.on('value', (snapshot) => {
+        let stafflist = snapshot.val();
+        //  console.log(stafflist);
+        let newState = [];
+        for (let staff in stafflist) {
+          newState.push({
+            id: staff,
+            initials: stafflist[staff].initials,
+            name : stafflist[staff].name,
+            designation : stafflist[staff].designation,
+            email : stafflist[staff].email,
+            phone : stafflist[staff].phone,
+            code : stafflist[staff].code
+          });
+        }
+        this.setState({
+          stafflist: newState
+        });
       });
-  //  }
-console.log(editState);
-    this.setState({
-      stafflist: editState
-    });
-  /*  itemRef.update({
-     '/name': this.state.stafflist.name
-    })
-*/
-    //console.log(itemRef.child);
-    //this.state.stafflist.name = itemRef.name;
-  // itemRef.remove();
-  }
+    }
 
-render(){
+  render() {
+    const brochure = this.state.stafflist.map((staff, index) => {
+      return (
+        <tr key={index}>
+                    <td>{staff.initials}</td>
+                    <td>{staff.name}</td>
+                    <td>{staff.designation}</td>
+                    <td>{staff.email}</td>
+                    <td>{staff.phone}</td>
+                    <td>{staff.code}</td>
+                  <td>
+                    <button className="btn btn-primary" data-toggle="modal" data-target="#exampleModal"
+                      onClick={() => this.replaceModalItem(index)}>edit</button> {" "}
+                  </td>
+                  <td>
+                    <td><button onClick={() => this.removeBranch(staff.id)}><span class="glyphicon glyphicon-trash"></span></button></td>
+                  </td>
+          </tr>
+      )
+    });
+    const warn =this.state.showWarning;
+    let modalData;
+    if (warn)
+    {
+      const requiredItem = this.state.requiredItem;
+      modalData = this.state.stafflist[requiredItem];
+      //console.log(modalData);
+    }
     return (
+
       <div>
-      <table border="1" className="table table-striped table-custom">
+        <div style={{ textAlign: "center" }}>
+          <h1>Staff List</h1>
+        </div>
+        <table className="table table-striped">
         <tr>
-            <td ><b>Initials</b></td>
-            <td><b>Name</b></td>
+            <td ><b>initials</b></td>
+            <td><b>name</b></td>
             <td><b>Designation</b></td>
             <td><b>Email</b></td>
             <td><b>Phone</b></td>
             <td><b>Code</b></td>
-            <td>&nbsp;</td>
-              <td>&nbsp;</td>
         </tr>
-          {this.state.stafflist.map((staff) => {
-            return (
-              <tr>
-                  <td>{staff.initials}</td>
-                  <td>{staff.name}</td>
-                  <td>{staff.designation}</td>
-                  <td>{staff.email}</td>
-                  <td>{staff.phone}</td>
-                  <td>{staff.code}</td>
-                  <td><button onClick={() => this.removeBranch(staff.id)}><span class="glyphicon glyphicon-trash"></span></button></td>
-                  <td>
-                      <button  onClick={() => this.editBranch(staff)}><span class="glyphicon glyphicon-edit"></span></button>
-                  </td>
-              </tr>
-            )
-          })}
-      </table>
-        </div>
-    )
+          <tbody>
+            {brochure}
+          </tbody>
+        </table>
+
+        {warn ? (
+          <Modal
+             initials= {modalData.initials}
+             name = {modalData.name}
+             designation = {modalData.designation}
+             email = {modalData.email}
+             phone = {modalData.phone}
+             code = {modalData.code }
+             id = {modalData.id}
+             saveModalDetails={this.saveModalDetails}
+          />
+              ) : (
+                console.log ('no data found')
+              )}
+      </div>
+    );
   }
 }
 
-export default StaffList
+export default StaffList;
