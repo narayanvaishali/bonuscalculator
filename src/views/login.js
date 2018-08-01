19235397854a6427996be6ff28a8ebb8b26d4795
Sabin Raj <sawbean.raz@googlemail.com>
2018-08-01 13:43:00 +0100

    Login Page

--- src/views/login.js
+++ src/views/login.js
@@ -1,47 +1,97 @@
-import React from "react";
+import React, { Component } from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Layout from "../components/Layout";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
-import InputAdornment from "@material-ui/core/InputAdornment";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 
-const Login = props => {
-  return (
-    <Layout>
-      <div
-        style={{
-          backgroundImage: `linear-gradient(rgb(148, 132, 223), rgba(13, 97, 146, 0.89))`,
-          minHeight: "100vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-      >
-        <Paper elevation={3} style={{ padding: "30px", minWidth: "400px" }}>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="email">Email</InputLabel>
-            <Input
-              id="email"
-              value=""
-              type="email"
-              // onChange={this.handleChange}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input
-              id="password"
-              value=""
-              type="password"
-              // onChange={this.handleChange}
-            />
-          </FormControl>
-        </Paper>
-      </div>
-    </Layout>
-  );
-};
+const styles = theme => ({
+  formControl: {
+    margin: theme.spacing.unit * 2
+  },
+  alignRight: {
+    textAlign: "right"
+  },
+  loginContainer: {
+    padding: "50px",
+    minWidth: "450px"
+  }
+});
 
-export default Login;
+class Login extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleLogin = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Layout>
+        <div
+          style={{
+            backgroundImage: `linear-gradient(rgb(148, 132, 223), rgba(13, 97, 146, 0.89))`,
+            minHeight: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Paper elevation={3} className={classes.loginContainer}>
+            <Typography variant="display2">Login</Typography>
+            <FormControl fullWidth className={classes.formControl}>
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <Input
+                id="email"
+                value={this.state.email}
+                type="email"
+                onChange={this.handleChange}
+              />
+            </FormControl>
+            <FormControl fullWidth className={classes.formControl}>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                id="password"
+                value={this.state.password}
+                type="password"
+                onChange={this.handleChange}
+              />
+            </FormControl>
+
+            <FormControl className={classes.formControl} fullWidth>
+              <div className={classes.alignRight}>
+                <Button variant="contained" color="primary">
+                  Sign In{" "}
+                </Button>
+                <Button variant="outlined" href={"/"}>
+                  Cancel
+                </Button>
+              </div>
+            </FormControl>
+          </Paper>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default withStyles(styles)(Login);
